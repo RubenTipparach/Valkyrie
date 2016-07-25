747554f988e88b407252f1474b666328a54ee2d6
Ruben Tipparach <santipab.stephane@gmail.com>
2016-07-24 22:04:01 -0500

    Commiting changes for big space GTA title.

--- Valkyrie_Bridge/assets/Scripts/ShipController.cs
+++ Valkyrie_Bridge/assets/Scripts/ShipController.cs
@@ -3,48 +3,69 @@ using System.Collections;
 
 public class ShipController : MonoBehaviour {
 
+	[SerializeField]
+	private float rotationalForce = 10000;
+
+	[SerializeField]
+	private float accelerationForce = 1000;
+
+	[SerializeField]
+	private float helperThrusterForce = 100;
+
+	public MeshCharacter characterPilot;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void ShipFixedUpdate () {
+
 		var rigidbody = this.GetComponent<Rigidbody>();
 		
 		if(Input.GetKey(KeyCode.A))
 		{
-			rigidbody.AddRelativeTorque(0, -10000, 0);
+			rigidbody.AddRelativeTorque(0, -1 * rotationalForce, 0);
 		}
 		if (Input.GetKey(KeyCode.W))
 		{
-			rigidbody.AddRelativeTorque(10000, 0, 0);
+			rigidbody.AddRelativeTorque(1 * rotationalForce, 0, 0);
 		}
 		if (Input.GetKey(KeyCode.S))
 		{
-			rigidbody.AddRelativeTorque(-10000, 0, 0);
+			rigidbody.AddRelativeTorque(-1 * rotationalForce, 0, 0);
 		}
 		if (Input.GetKey(KeyCode.D))
 		{
-			rigidbody.AddRelativeTorque(0,10000, 0);
+			rigidbody.AddRelativeTorque(0,1 * rotationalForce, 0);
+		}
+	
+		if (Input.GetKey(KeyCode.Q))
+		{
+			rigidbody.AddRelativeTorque(0, 0, 1 * rotationalForce);
+		}
+		if (Input.GetKey(KeyCode.E))
+		{
+			rigidbody.AddRelativeTorque(0,0, -1 * rotationalForce);
 		}
 
 		//translations
 		if (Input.GetKey(KeyCode.R))
 		{
-			rigidbody.AddRelativeForce(0, 100, 0);
+			rigidbody.AddRelativeForce(0, 1 * helperThrusterForce, 0);
 		}
 		if (Input.GetKey(KeyCode.F))
 		{
-			rigidbody.AddRelativeForce(0, -100, 0);
+			rigidbody.AddRelativeForce(0, -1 * helperThrusterForce, 0);
 		}
 		if (Input.GetKey(KeyCode.Z))
 		{
-			rigidbody.AddRelativeForce(-500, 0, 0);
+			rigidbody.AddRelativeForce(-5 * helperThrusterForce, 0, 0);
 		}
 		if (Input.GetKey(KeyCode.X))
 		{
-			rigidbody.AddRelativeForce(500, 0, 0);
+			rigidbody.AddRelativeForce(5* helperThrusterForce, 0, 0);
 		}
 
 
@@ -52,12 +73,12 @@ public class ShipController : MonoBehaviour {
 		//forward & back
 		if (Input.GetKey(KeyCode.LeftControl))
 		{
-			rigidbody.AddRelativeForce(0, 0, -500);
+			rigidbody.AddRelativeForce(0, 0, -.5f * accelerationForce);
 		}
 
 		if (Input.GetKey(KeyCode.Space))
 		{
-			rigidbody.AddRelativeForce(0, 0, 1000);
+			rigidbody.AddRelativeForce(0, 0, 1 * accelerationForce);
 		}
 	}
 }
